713ada772af8dfd3633946e4ffa0701893733e92
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-05-22 22:45:15 +0530

    set Matrix Zero study

---
+++ TwoDArrays/SetMatrixZeroes/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void setZeroes(int[][] matrix) {
+            
+    }
+    public static void main(String[] args){
+        int[][] matrix={{1,1,1},{1,0,1},{1,1,1}};
+        setZeroes(matrix);       
+    }
+}
