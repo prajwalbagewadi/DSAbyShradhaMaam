439660673b2bcd646d444ddd76f5a702456751f4
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-05-29 23:09:48 +0530

    Rotate Image Study

---
+++ TwoDArrays/Medium/RotateImage/Solution.java
@@ -0,0 +1,12 @@
+public class Solution{
+    public static void rotate(int[][] matrix) {
+        
+    }
+    public static void main(String[] args) {
+        // 1 2 3   
+        // 4 5 6   
+        // 7 8 9  
+        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+    }
+}
